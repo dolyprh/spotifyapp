81ad7c9824d11fc9015b7a852982df1aa8ee2efe
dolyprh <dolyprahoro@gmail.com>
2022-04-24 00:31:44 +0700

    change url

--- src/auth/config.js
+++ src/auth/config.js
@@ -1,6 +1,6 @@
 export const config = {
     CLIENT_ID : "f90b651c0c174c57b68739dbf330f7e4",
-    REDIRECT_URI : "http://",
+    REDIRECT_URI : "http://spotifyapp-gamma.vercel.app/",
     AUTH_ENDPOINT : "https://api.spotify.com/v1",
     RESPONSE_TYPE : "token",
     SCOPES : "playlist-modify-private",
